7bf1b06f438fde7ec2885fa8f56c1494cf1e5bb1
Jon McClure <jmcclure@politico.com>
2019-01-16 15:53:27 -0500

    auth and permission viewset classes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="django-politico-staff",
-    version="0.0.2",
+    version="0.0.3",
     description="",
     url="https://github.com/The-Politico/django-politico-staff",
     author="POLITICO interactive news",
--- staff/viewsets/staffer.py
+++ staff/viewsets/staffer.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
+from staff.conf import settings
 from staff.serializers import UserSerializer
 
 
 class StafferViewSet(ReadOnlyModelViewSet):
     serializer_class = UserSerializer
+    authentication_classes = (settings.API_AUTHENTICATION_CLASS,)
+    permission_classes = (settings.API_PERMISSION_CLASS,)
 
     def get_queryset(self):
         queryset = User.objects.filter(is_staff=True)
