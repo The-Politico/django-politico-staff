6f3383044176aa338323760d6b55c409b8b434b8
Jon McClure <jmcclure@politico.com>
2019-02-04 12:55:03 -0500

    run slack sync on user creation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="django-politico-staff",
-    version="0.0.9",
+    version="0.0.10",
     description="",
     url="https://github.com/The-Politico/django-politico-staff",
     author="POLITICO interactive news",
