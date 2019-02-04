24cd05ffa94ea91f2fae99543d0449b1e1e88bb3
Jon McClure <jmcclure@politico.com>
2019-02-04 12:29:48 -0500

    push 0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="django-politico-staff",
-    version="0.0.6",
+    version="0.0.7",
     description="",
     url="https://github.com/The-Politico/django-politico-staff",
     author="POLITICO interactive news",
